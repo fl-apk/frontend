13ab1d1bce6cde9e8a05631a176765f993222493
Pranav Kumar <17pranav.k@gmail.com>
2022-12-07 21:23:52 +0530

    navbar added

--- fl-frontend/src/App.js
+++ fl-frontend/src/App.js
@@ -3,31 +3,34 @@ import Home from './pages/Home';
 import FedL from './pages/FedL';
 import Stats from './pages/Stats';
 import Validation from './pages/Validation';
+import NavBar from './components/Navbar';
 import videoBg from "./ambulance.mp4";
 import { SocketContext, socket } from './context/socket';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <SocketContext.Provider value={socket}>
-      <BrowserRouter>
-        <div>
-          <div className="main">
-            <div className="overlay"></div>
-            <video src={videoBg} autoPlay loop muted />
-            <div className="content">
-              <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/fed" element={<FedL />} />
-                <Route exact path="/stats" element={<Stats />} />
-                <Route exact path="/validation" element={<Validation />} />
-              </Routes>
+    <div className="app">
+      <SocketContext.Provider value={socket}>
+        <BrowserRouter>
+          <div>
+            <NavBar />
+            <div className="main">
+              <div className="overlay"></div>
+              <video src={videoBg} autoPlay loop muted />
+              <div className="content">
+                <Routes>
+                  <Route exact path="/" element={<Home />} />
+                  <Route exact path="/fed" element={<FedL />} />
+                  <Route exact path="/stats" element={<Stats />} />
+                  <Route exact path="/validation" element={<Validation />} />
+                </Routes>
+              </div>
             </div>
           </div>
-        </div>
-      </BrowserRouter>
-    </SocketContext.Provider>
-
+        </BrowserRouter>
+      </SocketContext.Provider>
+    </div>
   );
 }
 
